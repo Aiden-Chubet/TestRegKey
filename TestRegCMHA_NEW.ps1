8d3e37d7ba979fc653d9c187fc627aec3b333209
Aiden.Chubet <147650530+ach-u-be14@users.noreply.github.com>
2023-10-18 13:16:55 -0400

    Script

---
+++ TestRegCMHA_NEW.ps1
@@ -0,0 +1,10 @@
+﻿$regPath = "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\CMHA_NEW"
+$value = Test-Path $regPath
+if($value){
+        Write-host "Reg Key already Exists. No Action Required"
+ }
+Else{
+        Remove-Item -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\* -Force -Recurse
+        
+        New-Item -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\CMHA_NEW -Force -Recurse
+}
